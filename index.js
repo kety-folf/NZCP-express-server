119a09d37ceae2fd66bb1ff7c2150bc21d4c1df1
kety-folf <keatonstorer@gmail.com>
2021-11-20 11:13:26 +1300

    clean up code

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get("/verify", async (req, res) => {
     */
     var code = JSON.stringify(req.query.token);
    // const trustedIssuers =  ["did:web:nzcp.identity.health.nz","did:web:nzcp.covid19.health.nz"] uncomment if testing and comment line below
-    const trustedIssuers =  ["c"]
+    //const trustedIssuers =  ["did:web:nzcp.identity.health.nz"]
     try {
         let result = await verifyPassURI(code, {trustedIssuer: "did:web:nzcp.identity.health.nz"});
         

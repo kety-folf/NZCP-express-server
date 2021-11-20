66bff3ed5f8488e61a3308084d693f0e9153d551
kety-folf <keatonstorer@gmail.com>
2021-11-21 01:26:27 +1300

    update to make it work fully offline

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {verifyPassURI, verifyPassURIWithTrustedIssuers} = require('@vaxxnz/nzcp');
+const {verifyPassURI, verifyPassURIOffline} = require('@vaxxnz/nzcp');
 const express = require("express");
 var bp = require('body-parser')
 const app = express();
@@ -9,24 +9,15 @@ app.use(express.json());
 
 
 app.get("/verify", async (req, res) => {
-    //only accept https
-    /*
-    if (req.protocol !== 'https') {
-        res.status(400).send('https only');
-        return;
-    }
-    */
     var code = JSON.stringify(req.query.token);
-   // const trustedIssuers =  ["did:web:nzcp.identity.health.nz","did:web:nzcp.covid19.health.nz"] uncomment if testing and comment line below
-    //const trustedIssuers =  ["did:web:nzcp.identity.health.nz"]
     try {
-        let result = await verifyPassURI(code, {trustedIssuer: "did:web:nzcp.identity.health.nz"});
+        let result = await verifyPassURIOffline(code);
         
        
         res.send(result);
         
     } catch (err) {
-      //  res.send(err);
+        res.send(err);
         console.log(err, "error");
     }
 });
